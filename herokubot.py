680386895051ad0de017573aae0b53190a0c27bf
Falconwatch <garrikwower@gmail.com>
2019-11-20 22:30:41 +0000

    add yandex translate

--- herokubot.py
+++ herokubot.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import requests
+import json
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -7,13 +9,21 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 def start(bot, update):
     update.effective_message.reply_text("Hi!")
 
-
 def echo(bot, update):
-    update.effective_message.reply_text(update.effective_message.text)
+    update.effective_message.reply_text(yandex(update.effective_message.text))
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+def yandex(message):
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
+    key = 'trnsl.1.1.20191117T205104Z.3ecf8e2efab662cd.262088c507c9012d2a5895fcfbc5624a17dd65ac'
+    text = message.text
+    lang = 'ru-en'
+
+    r = requests.post(url, data={'key': key, 'text': text, 'lang': lang})
+    result = json.loads(r.text)
+    return ' '.join(result['text'])
 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
